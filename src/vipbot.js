e791d8afea8e2de5b25224e9640a7763432b8be9
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-01-28 02:55:46 -0300

    Add files via upload

---
+++ src/vipbot.js
@@ -0,0 +1,19 @@
+const vipbot = (prefix) => {
+    return `馃拵 Brizas-bot vip exclusivo馃拵
+馃敯 *TABELA DE PRE脟OS* 馃敯
+
+馃帀Ensinar a baixar e manusear o bot pelo termux: R$: 35,00 馃帀
+
+馃帀Adicionar comandos de imagens, contato, video, audio, texto, etc...: R$: 12,00 馃帀
+
+馃帀Criar um n煤mero exclusivo para o bot: R$: 7,00 馃帀
+
+馃帀 C贸digo original descriptografado do bot: R$: 300,00 馃帀
+
+馃帀 Outros comandos: negocie com o criador pra ver se 茅 possivel colocar 馃帀
+
+馃敯 *MAIS INFORMA脟脮ES* 馃敯 Ajuda O CaRa Q Criou O Script Brow😎🤙
+
+D锚: ${prefix}criador para negociar`
+}
+exports.vipbot = vipbot
